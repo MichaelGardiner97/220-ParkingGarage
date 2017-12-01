4bb052016b93f8ea18db39047cd981b22a7e4c0f
Dhowes24 <33637595+Dhowes24@users.noreply.github.com>
2017-11-30 21:49:19 -0500

    Add files via upload
    Spot Node first upload

---
+++ SpotNode.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Derek Howes on 11/30/17.
+//
+#include "SpotNode.h"
+
+void CarNode::checkoutCar(std::string name){
+    //check if this->taken = false
+
+    //else if check name
+        //call worker returnToClient function (carNode:: carNode* car)
+        //car = nullptr
+
+}
+
+void CarNode::checkinCar(carNode:: carNode* car){
+    //vehicle ptr = car
+}
+
---
+++ SpotNode.h
@@ -0,0 +1,22 @@
+//
+// Created by Derek Howes on 11/30/17.
+//
+
+#ifndef TERMPROJECT_SPOTNODE_H
+#define TERMPROJECT_SPOTNODE_H
+
+#endif //TERMPROJECT_SPOTNODE_H
+
+class SpotNode{
+private:
+    int vehicleType;
+    std::string postion;
+    bool taken;
+    CarNode* vehicle;
+
+public:
+    void checkoutCar(std::string name);
+    //does all the checks
+    void checkinCar(CarNode Car);
+    //moves pointer to car
+};

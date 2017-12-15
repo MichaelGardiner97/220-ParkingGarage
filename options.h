f425cf1ea8056f214ca94ee1ee4b7ecce2238b25
esteele104 <33662907+esteele104@users.noreply.github.com>
2017-12-15 12:36:45 -0500

    Add files via upload

--- options.h
+++ options.h
@@ -7,8 +7,12 @@
 #include "WorkerList.h"
 
 void printOptions();
+void checkOption(std::string input);
+void selectOption( int x, int y);
 void logIn(WorkerList* notAvail, WorkerList* avail);
 void managerLogin(WorkerList* avail, WorkerList* notAvail);
+void updateLists(WorkerNode* worker, WorkerList* avail, WorkerList* notAvail);
+
 
 
 #endif //PARKINGGARAGE_OPTIONS_H
